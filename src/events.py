30fd45d9b0b555fa4c40b6cf9bb17ad21a366d6d
Seth Hinz <sethhinz@me.com>
2024-03-04 21:46:41 -0800

    Cleanup events.py

--- src/events.py
+++ src/events.py
@@ -1,5 +1,10 @@
 from pygame.event import custom_type
 
+# Dispatched from the Setup state when the game should start.
 START_PONG = custom_type()
+
+# Dispatched from teh Pong state when the user first hits the ball. This is used for aesthetics only (the music starts).
 FIRST_HIT = custom_type()
+
+# Dispatched when the ball leaves the pong game.
 GAME_OVER = custom_type()
